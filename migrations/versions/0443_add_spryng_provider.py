25e7aada86dba069bc7e7a909dae51842806cf7d
Pier-Angelo Gaetani <pier.gaetani@gmail.com>
2024-05-07 14:43:44 +0200

    spryng migration version fix

--- migrations/versions/0443_add_spryng_provider.py
+++ migrations/versions/0443_add_spryng_provider.py
@@ -7,6 +7,7 @@ Create Date: 2024-05-07 13:05:00.000000
 """
 
 import uuid
+from datetime import datetime
 
 from alembic import op
 
@@ -15,23 +16,29 @@ revision = "0443_add_spryng_provider"
 down_revision = "0442_new_sms_allowance_n_rate"
 
 
+identifier = "spryng"
+
+
 def upgrade():
+    provider_id = str(uuid.uuid4())
     op.execute(
-        "INSERT INTO provider_details (id, display_name, identifier, priority, notification_type, active) values ('{}', 'Spryng', 'spryng', 30, 'sms', true)".format(
-            str(uuid.uuid4())
-        )
+        f"INSERT INTO provider_details (id, display_name, identifier, priority, notification_type, active, version) values ('{identifier.capitalize()}', 'Spryng', '{identifier}', 30, 'sms', true, 1)"
     )
 
     op.execute(
-        "UPDATE provider_rates set provider_id = (select id from provider_details where identifier = 'spryng') where provider = 'spryng'"
+        f"INSERT INTO provider_rates (id, valid_from, rate, provider_id) VALUES ('{uuid.uuid4()}', '{datetime.utcnow()}', 1.0, '{provider_id}')"
     )
 
     op.execute(
-        "UPDATE provider_statistics set provider_id = (select id from provider_details where identifier = 'spryng') where provider = 'spryng'"
+        f"UPDATE provider_rates set provider_id = (select id from provider_details where identifier = '{identifier}') where provider = '{identifier}'"
+    )
+
+    op.execute(
+        f"UPDATE provider_statistics set provider_id = (select id from provider_details where identifier = '{identifier}') where provider = '{identifier}'"
     )
 
 
 def downgrade():
-    op.execute("DELETE FROM provider_statistics WHERE provider = 'spryng'")
-    op.execute("DELETE FROM provider_rates WHERE provider = 'spryng'")
-    op.execute("DELETE FROM provider_details WHERE identifier = 'spryng'")
+    op.execute(f"DELETE FROM provider_statistics WHERE provider = '{identifier}'")
+    op.execute(f"DELETE FROM provider_rates WHERE provider = '{identifier}'")
+    op.execute(f"DELETE FROM provider_details WHERE identifier = '{identifier}'")
