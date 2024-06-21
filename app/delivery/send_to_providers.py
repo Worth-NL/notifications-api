25628b6111ba225a3903855106415d9c258c38c9
Pier-Angelo Gaetani <pier.gaetani@gmail.com>
2024-06-21 14:20:52 +0200

    log level

--- app/delivery/send_to_providers.py
+++ app/delivery/send_to_providers.py
@@ -139,7 +139,7 @@ def send_email_to_provider(notification):
                 current_app.logger.warning(
                     "!!! reply_to used :: %s :: %s:: %s", reply_to, type(reply_to), len(reply_to)
                 )
-                # from_email_address = reply_to if len(reply_to) < 2 else reply_to[0]
+                from_email_address = reply_to if len(reply_to) < 2 else reply_to[0]
 
             from_address = f'"{email_sender_name}" <{from_email_address}>'
 
